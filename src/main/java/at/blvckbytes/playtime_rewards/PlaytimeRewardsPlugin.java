25665f393966bdcb5dcbb746792d0d0131511cf2
BlvckBytes <blvckbytes@gmail.com>
2026-05-10 23:03:40 +0200

    fixed tab-completers not being set properly; fixed config-reloads not taking effect on commands

--- src/main/java/at/blvckbytes/playtime_rewards/PlaytimeRewardsPlugin.java
+++ src/main/java/at/blvckbytes/playtime_rewards/PlaytimeRewardsPlugin.java
@@ -2,6 +2,7 @@ package at.blvckbytes.playtime_rewards;
 
 import at.blvckbytes.cm_mapper.ConfigHandler;
 import at.blvckbytes.cm_mapper.ConfigKeeper;
+import at.blvckbytes.cm_mapper.ReloadPriority;
 import at.blvckbytes.cm_mapper.section.command.CommandUpdater;
 import at.blvckbytes.playtime_rewards.command.*;
 import at.blvckbytes.playtime_rewards.command.main.MainCommandSection;
@@ -22,6 +23,9 @@ import at.blvckbytes.playtime_rewards.store.UserDataStore;
 import net.ess3.api.IEssentials;
 import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Nullable;
 
@@ -70,19 +74,19 @@ public class PlaytimeRewardsPlugin extends JavaPlugin {
       getServer().getPluginManager().registerEvents(rewardsDisplayHandler, this);
 
       var playtimeCommand = Objects.requireNonNull(getCommand(PlaytimeCommandSection.INITIAL_NAME));
-      playtimeCommand.setExecutor(new PlaytimeCommand(userDataStore, offlinePlayerRegistry, config));
+      setExecutorAndCompleter(playtimeCommand, new PlaytimeCommand(userDataStore, offlinePlayerRegistry, config));
 
       var playTopCommand = Objects.requireNonNull(getCommand(PlayTopCommandSection.INITIAL_NAME));
-      playTopCommand.setExecutor(new PlayTopCommand(userDataStore, config));
+      setExecutorAndCompleter(playTopCommand, new PlayTopCommand(userDataStore, config));
 
       var afkTopCommand = Objects.requireNonNull(getCommand(AfkTopCommandSection.INITIAL_NAME));
-      afkTopCommand.setExecutor(new AfkTopCommand(userDataStore, config));
+      setExecutorAndCompleter(afkTopCommand, new AfkTopCommand(userDataStore, config));
 
       var rewardsCommand = Objects.requireNonNull(getCommand(RewardsCommandSection.INITIAL_NAME));
-      rewardsCommand.setExecutor(new RewardsCommand(userDataStore, rewardsDisplayHandler, offlinePlayerRegistry, config));
+      setExecutorAndCompleter(rewardsCommand, new RewardsCommand(userDataStore, rewardsDisplayHandler, offlinePlayerRegistry, config));
 
       var mainCommand = Objects.requireNonNull(getCommand(MainCommandSection.INITIAL_NAME));
-      mainCommand.setExecutor(new MainCommand(offlinePlayerRegistry, userDataStore, calendarInfoProvider, config, this));
+      setExecutorAndCompleter(mainCommand, new MainCommand(offlinePlayerRegistry, userDataStore, calendarInfoProvider, config, this));
 
       var commandUpdater = new CommandUpdater(this);
 
@@ -96,6 +100,9 @@ public class PlaytimeRewardsPlugin extends JavaPlugin {
 
       updateCommands.run();
       commandUpdater.trySyncCommands();
+
+      config.registerReloadListener(updateCommands);
+      config.registerReloadListener(commandUpdater::trySyncCommands, ReloadPriority.LOWEST);
     } catch (Throwable e) {
       logger.log(Level.SEVERE, "An error occurred while trying to enable the plugin; disabling!", e);
       Bukkit.getPluginManager().disablePlugin(this);
@@ -121,5 +128,12 @@ public class PlaytimeRewardsPlugin extends JavaPlugin {
     } catch (Throwable e) {
       getLogger().log(Level.SEVERE, "An internal error occurred", e);
     }
+  }
+
+  private void setExecutorAndCompleter(PluginCommand command, CommandExecutor executor) {
+    command.setExecutor(executor);
+
+    if (executor instanceof TabCompleter tabCompleter)
+      command.setTabCompleter(tabCompleter);
   }
 }
